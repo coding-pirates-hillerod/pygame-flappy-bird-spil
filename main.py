09edcd07b7bc32a37f63bb86be21539537f55ba7
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-06-24 20:26:26 +0200

    Added flappy bird

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Any
 import pygame
 from pygame.locals import *
 
@@ -20,6 +21,45 @@ scroll_speed = 4
 bg = pygame.image.load("./img/bg.png")
 ground_img = pygame.image.load("./img/ground.png")
 
+
+# flappy bird class
+class Bird(pygame.sprite.Sprite):
+    def __init__(self, x: int, y: int) -> None:
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        self.index = 0
+        self.counter = 0
+
+        # load all flappy bird imgages
+        for num in range(1, 4):
+            img = pygame.image.load(f"./img/bird{num}.png")
+            self.images.append(img)
+
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+
+    def update(self):
+        # handle the animation
+        self.counter += 1
+        flap_cooldown = 5
+
+        if self.counter > flap_cooldown:
+            self.counter = 0
+            self.index += 1
+
+            if self.index >= len(self.images):
+                self.index = 0
+
+        self.image = self.images[self.index]
+
+
+bird_group = pygame.sprite.Group()
+
+flappy = Bird(100, int(screen_height / 2))
+
+bird_group.add(flappy)
+
 run = True
 while run:
 
@@ -27,6 +67,12 @@ while run:
 
     # Draw background
     screen.blit(bg, (0, 0))
+
+    # draw bird
+    bird_group.draw(screen)
+
+    # update bird
+    bird_group.update()
 
     # Draw and scroll the ground
     screen.blit(ground_img, (ground_scroll, 768))
