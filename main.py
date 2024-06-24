52536ac28803ff7a45a52feb5716b746dd57d2c3
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-06-24 17:03:55 +0200

    Added background image

--- main.py
+++ main.py
@@ -7,10 +7,18 @@ screen_height = 936
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Flappy Bird")
 
+# load images
+bg = pygame.image.load("./img/bg.png")
+
 run = True
 while run:
+
+    screen.blit(bg, (0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    pygame.display.update()
 
 pygame.quit()
