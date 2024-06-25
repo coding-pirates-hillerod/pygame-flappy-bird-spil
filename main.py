5859cf52bd432e80f77727ed6c384d4900fbc40a
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-06-26 00:45:17 +0200

    Finished part 6

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ font = pygame.font.SysFont("Bauhaus 93", 60)
 # define colour
 white = (255, 255, 255)
 
-
 # game variables
 ground_scroll = 0
 scroll_speed = 4
@@ -34,11 +33,20 @@ pass_pipe = False
 # load images
 bg = pygame.image.load("./img/bg.png")
 ground_img = pygame.image.load("./img/ground.png")
+button_img = pygame.image.load("./img/restart.png")
 
 
 def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x, y))
+
+
+def reset_game():
+    pipe_group.empty()
+    flappy.rect.x = 100
+    flappy.rect.y = int(screen_height / 2)
+    score = 0
+    return score
 
 
 # flappy bird class
@@ -117,6 +125,29 @@ class Pipe(pygame.sprite.Sprite):
             self.kill()
 
 
+class Button:
+    def __init__(self, x, y, image) -> None:
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+    def draw(self):
+        action = False
+
+        # get mouse position
+        pos = pygame.mouse.get_pos()
+
+        # check if mouse is over the button
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1:
+                action = True
+
+        # draw button
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+        return action
+
+
 # sprites
 flappy = Bird(100, int(screen_height / 2))
 
@@ -125,6 +156,9 @@ bird_group = pygame.sprite.Group()
 bird_group.add(flappy)
 
 pipe_group = pygame.sprite.Group()
+
+# restart button instance
+button = Button(screen_width // 2 - 50, screen_height // 2 - 100, button_img)
 
 run = True
 while run:
@@ -188,6 +222,12 @@ while run:
             ground_scroll = 0
 
         pipe_group.update()
+
+    # check for game over and reset
+    if game_over == True:
+        if button.draw() == True:
+            game_over = False
+            score = reset_game()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
